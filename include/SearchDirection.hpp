b1a86cc01a778d9100b1c851745e43d692e64009
YuitoTakeuchi <tyuito0913@gmail.com>
2023-05-28 23:19:17 +0900

    feat: implement conjugate gradient descent

--- include/SearchDirection.hpp
+++ include/SearchDirection.hpp
@@ -16,6 +16,10 @@ protected:
     :objective_func(objective_func), gradient_func(gradient_func)  {
 
     }
+
+public:
+    Eigen::VectorXd get_optimal_point() {return optimal_point;};
+    double get_optimal_value() {return optimal_function_value;};
 };
 
 template<class LineSearchAlgorithm>
@@ -47,7 +51,55 @@ public:
         optimal_point = x;
         optimal_function_value = objective_func(x);
     };
+};
 
-    Eigen::VectorXd get_optimal_point() {return optimal_point;};
-    double get_optimal_value() {return optimal_function_value;};
+template<class LineSearchAlgorithm>
+class ConjugateGradient: public SearchDirection {
+private:
+
+
+public:
+    ConjugateGradient(double (*objective_func)(const Eigen::VectorXd&), Eigen::VectorXd (*gradient_func)(const Eigen::VectorXd&))
+    : SearchDirection(objective_func, gradient_func) {
+
+    }
+    void solve(Eigen::VectorXd x, double tolerance=1e-9) {
+        const int N = x.rows();
+        Eigen::VectorXd grad, search_direction;
+        search_direction = Eigen::VectorXd::Zero(N);
+        grad = Eigen::VectorXd::Ones(N);
+        double alpha = 1.0;
+        int cnt = 0;
+        bool reset = false;
+
+        double grad_squared_norm = 1.0, prev_grad_squared_norm = 1.0;
+        double beta = 0.0;
+        while(grad.maxCoeff() > tolerance || -grad.minCoeff() > tolerance) {
+            grad = gradient_func(x);
+
+            prev_grad_squared_norm = grad_squared_norm;
+            grad_squared_norm = grad.squaredNorm();
+
+            if(reset) {
+                beta = 0;
+                reset = false;
+            } else {
+                beta = grad_squared_norm / prev_grad_squared_norm;
+            }
+
+            if(beta > 0.10) {
+                reset = true;
+            }
+
+            search_direction = - grad / sqrt(grad_squared_norm) + beta * search_direction;
+            double grad_phi0 = gradient_func(x).dot(search_direction);
+
+            LineSearchAlgorithm ls(objective_func, gradient_func, objective_func(x), grad_phi0, x, search_direction, alpha);
+
+            x += ls.find_step() * search_direction;
+            ++cnt;
+        }
+        optimal_point = x;
+        optimal_function_value = objective_func(x);
+    };
 };
